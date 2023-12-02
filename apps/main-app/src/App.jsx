8599bef901ee282ceea5e14680a07f5370d2423a
Woongjae Lee <2woongjae@gmail.com>
2023-12-02 22:57:33 +0900

    step4: Lazy, Suspense, ErrorBoundary 를 통한 통합

--- apps/main-app/src/App.jsx
+++ apps/main-app/src/App.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import Button from "component_app/Button";
 import { map, join } from "lodash";
 import { NameProvider } from "shared-library";
+import { ErrorBoundary } from "react-error-boundary";
+
+const Button = React.lazy(() => import("component_app/Button"));
 
 const App = () => (
   <NameProvider name="hello">
@@ -15,14 +17,22 @@ const App = () => (
       <div>Language: JavaScript</div>
       <div>CSS: Empty CSS</div>
       <div>{join(map(["1", "2"]), "-")}</div>
-      <Button
-        onClick={() => {
-          console.log("Clicked!!");
-        }}
-      >
-        Primary
-      </Button>
-      <Button type="warning">Warning</Button>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Button
+            onClick={() => {
+              console.log("Clicked!!");
+            }}
+          >
+            Primary
+          </Button>
+        </React.Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Button type="warning">Warning</Button>
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
   </NameProvider>
 );
